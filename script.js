3622917b5f96d5a7549dde365e1ce7edcbabb0c9
knika-starkey <knika787@gmail.com>
2022-09-22 23:03:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let m = +prompt("Enter number");
+let n = +prompt("Enter number");
+if (isNaN(n)) {
+  alert("IT IS NOT A NUMBER");
+} else if (n == 0 || m == 0) {
+  alert("it's zero");
+} else {
+  let m1 = m;
+  let n1 = n;
+  while (m1 !== n1) {
+    if (m1 > n1) m1 -= n1;
+    else n1 -= m1;
+  }
+  //   if (~~(m / n) > 0 && m % n > 0) alert(`${~~(m / n)} ${m % n}/${n / n1}`);
+  //   else if (m % n == 0 || n == 1) alert(m / m1);
+  //   else alert(m / m1 + "/" + n / n1);
+  alert(m / m1 + "/" + n / n1);
+}
